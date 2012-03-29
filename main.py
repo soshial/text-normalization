772969ff34b45886d48b63a9d9ca7cc3645a2506
soshial <soshial.reg@gmail.com>
2012-03-29 13:42:12 +0400

    more cases of converting into numbers covered; exceptions changed; added a stub for numword for RUSSIAN numbers

--- main.py
+++ main.py
@@ -15,21 +15,34 @@ def check_and_convert_into_number(str):
     import re
     if not re.search("\d",str): # if numbers are not present then we just return the string back
         return str
-    # todo "1982-95" -> "from 1982 to 1995"
-    # todo 4%
-    # todo "13-year-olds"
     clean_number_string = re.sub("[^\d.-]","",str) # cleaned number with minus, dots etc: # -30 ,.3879 -> -30.3879
     try:
         canonical_number = get_canonical_number_from_string(clean_number_string) # cleaned number converting into long/float
-    except ValueError:
+    except StandardError:
         logger.info('Problem with processing type of variable (long, float): ' + str + ' -> ' + clean_number_string)
-        return ''
+        return '' #canonical_number = get_canonical_number_from_string(re.sub("[^\d]","",str))
     # main processing of the word
     try:
         decades = {"'20s":"twenties","1920s":"twenties","'30s":"thirties","1930s":"thirties","'40s":"fourties","1940s":"fourties",
                  "'50s":"fifties","1950s":"fifties","'60s":"sixties","1960s":"sixties","'70s":"seventies","1970s":"seventies",
                  "'80s":"eighties","1980s":"eighties","'90s":"nineties","1990s":"nineties"}
-        if re.search("^[\d. -]+$",str): # if we have just clean numbers with math symbols or spaces
+        if re.search("^\d{4}-\d{2,4}$",str): # "1982-(19)95" -> "from 1982 to 1995"
+            def daterepl(matchobj):
+                try:
+                    return "from " + numword_en.cardinal(get_canonical_number_from_string(matchobj.group(1))) +" to " + numword_en.cardinal(get_canonical_number_from_string(matchobj.group(2)))
+                except StandardError:
+                    logger.info('Problem with processing type of 2 variables (long, float): ' + str)
+                    return ''
+            return re.sub("(\d{4})-(\d{4})",daterepl,str,0)
+        elif re.search("(\d\d\d\d-\d\d-\d\d)|(\d\d-\d\d-\d\d\d\d)|(\d\d\d\d/\d\d/\d\d)|(\d\d/\d\d/\d\d\d\d)",str):
+            return ' ' # 2011-10-17, 12/02/1997 are omitted, but the sentences are not
+        elif re.search("^(\+?\d?[\(]\d{3}[\)][\.| |\-]?|^\d{3}[\.|\-| ]?)?\d{3}(\.|\-| )?\d\d(\.|\-| )?\d\d$",str):
+            print 'phone';return '' # the phone numbers like (607)-432-1000 (916) 934-45-54
+        elif re.search("^\d+(-year-old|-pound|-foot|-acre|-year|-liter|-litre|-step|-yard|-day|-hour|-month-old|-month|-million|-week|-mile|-plus|-point|-minute|-inch|-degrees|-second)$",str): #
+            return re.sub("^\d+",numword_en.cardinal(canonical_number),str)
+        elif re.search("^\d+%|‰|‱$",str): # todo per mille, basis point
+            return numword_en.ordinal(canonical_number) + ' percent'
+        elif re.search("^[\d. -]+$",str): # if we have just clean numbers with math symbols or spaces
             return numword_en.cardinal(canonical_number)
         elif re.search("^\d+(th|rd|nd|st)$",str):
             return numword_en.ordinal(canonical_number)
@@ -41,7 +54,7 @@ def check_and_convert_into_number(str):
             return numword_en.cardinal(canonical_number*1000000)
         else:
             return re.sub("[\d.-]+",numword_en.cardinal(canonical_number),str)
-    except TypeError, error_message:
+    except StandardError, error_message: # todo test TypeError
         logger.info('Problem with handling type of number: ' + unicode(error_message) +'___'+ str + ' -> ' + clean_number_string)
         return ''
 
@@ -58,7 +71,7 @@ def dictionary_check(word):
         i += 1
     return (variants & dictionary)[0]
 
-# initializing logging todo fix log permissions
+# initializing logging
 import logging
 logger = logging.getLogger('norm')
 hdlr = logging.FileHandler('./norm.log')
@@ -78,7 +91,7 @@ cursor = conn.cursor ()
 principal_words = {}
 other_words = {}
 cursor.execute ("SELECT word_id, word_spelling, principal FROM word_equivalents WHERE lang = 'en' AND relation = 1 AND principal <> -1")
-while (1):
+while True:
     row = cursor.fetchone ()
     if row == None:
         break
@@ -88,7 +101,7 @@ logger.info("Equivalent words imported: %d" % cursor.rowcount)
 
 dictionary = set()
 cursor.execute ("SELECT word FROM en_us")
-while (1):
+while True:
     row = cursor.fetchone ()
     if row == None:
         break
@@ -115,7 +128,7 @@ import glob, os, codecs
 
 import time
 wait = 0.01
-
+# todo cut in the end of file <span> </>
 # main loop
 while True:
     for fullpath_inmeta in glob.glob( os.path.join(path_in, '*.meta') ):
@@ -158,7 +171,12 @@ while True:
 
 #canonicalization: "co-operation" → "cooperation", "valour" → "valor", "should've" → "should have" / redis?
 
-#punctuation removal todo ?
+#punctuation removal
 #puncts='.?!'
 #for sym in puncts:
 #    text= text.replace(sym,' ')
+
+# NER
+# tagger = nltk.data.load('chunkers/maxent_ne_chunker/english_ace.pickle')
+# tagger.parse([('Guido', 'NNP'), ('lives', 'NNS'), ('in', 'IN'), ('Seattle', 'NNP')] )
+# gives: Tree('S', [Tree('NE', [('Guido', 'NNP')]), ('lives', 'NNS'), ('in', 'IN'), Tree('NE', [('Seattle', 'NNP')])])
---
+++ numword/numword_ru.py
@@ -0,0 +1,184 @@
+# coding: utf-8
+#This file is part of numword.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
+"""
+numword for RU
+"""
+
+from numword_eu import NumWordEU
+
+
+class NumWordEN(NumWordEU):
+    """
+    NumWord EN
+    """
+
+    def _set_high_numwords(self, high):
+        """
+        Set high num words
+        """
+        max_val = 3 + 3 * len(high)
+        for word, i in zip(high, range(max_val, 3, -3)):
+            self.cards[10**i] = word + u"illion"
+
+    def _setup(self):
+        """
+        Setup
+        """
+        self.negword = u"minus "
+        self.pointword = u"point"
+        self.errmsg_nonnum = "Only numbers may be converted to words."
+        self.exclude_title = [u"and", u"point", u"minus"]
+
+        self.mid_numwords = [(1000, u"тысяча"), (100, u"hundred"),
+                (90, u"ninety"), (80, u"eighty"), (70, u"seventy"),
+                (60, u"sixty"), (50, u"fifty"), (40, u"forty"), (30, u"thirty")]
+        self.low_numwords = [u"twenty", u"nineteen", u"eighteen", u"seventeen",
+                u"sixteen", u"fifteen", u"fourteen", u"thirteen", u"twelve",
+                u"eleven", u"ten", u"nine", u"eight", u"seven", u"six", u"five",
+                u"four", u"three", u"two", u"one", u"zero"]
+        self.ords = {
+                u"one": u"first",
+                u"two": u"second",
+                u"three": u"third",
+                u"five": u"fifth",
+                u"eight": u"eighth",
+                u"nine": u"ninth",
+                u"twelve": u"twelfth",
+                }
+
+
+    def _merge(self, curr, next):
+        """
+        Merge
+        """
+        ctext, cnum, ntext, nnum = curr + next
+
+        if cnum == 1 and nnum < 100:
+            return next
+        elif 100 > cnum > nnum :
+            return (u"%s-%s" % (ctext, ntext), cnum + nnum)
+        elif cnum >= 100 > nnum:
+            return (u"%s and %s" % (ctext, ntext), cnum + nnum)
+        elif nnum > cnum:
+            return (u"%s %s" % (ctext, ntext), cnum * nnum)
+        return (u"%s, %s" % (ctext, ntext), cnum + nnum)
+
+
+    def ordinal(self, value):
+        """
+        Convert to ordinal
+        """
+        self._verify_ordinal(value)
+        outwords = self.cardinal(value).split(" ")
+        lastwords = outwords[-1].split("-")
+        lastword = lastwords[-1].lower()
+        try:
+            lastword = self.ords[lastword]
+        except KeyError:
+            if lastword[-1] == u"y":
+                lastword = lastword[:-1] + u"ie"
+            lastword += u"th"
+        lastwords[-1] = self._title(lastword)
+        outwords[-1] = u"-".join(lastwords)
+        return " ".join(outwords)
+
+
+    def ordinal_number(self, value):
+        """
+        Convert to ordinal num
+        """
+        self._verify_ordinal(value)
+        return u"%s%s" % (value, self.ordinal(value)[-2:])
+
+
+    def year(self, val, longval=True):
+        """Convert number into year"""
+        if not (val//100)%10: # years like 1066 or 2011 are treated as cardinal
+            return self.cardinal(val)
+        elif 1700 <= val <= 2050: # years in these borders are usually spelled without joining words
+            return self._split(val, hightxt=u"", jointxt=u"", longval=longval)
+        return self._split(val, hightxt=u"hundred", jointxt=u"and", longval=longval)
+
+    def currency(self, val, longval=True):
+        """
+        Convert to currency
+        """
+        return self._split(val, hightxt=u"dollar/s", lowtxt=u"cent/s",
+                                jointxt=u"and", longval=longval)
+
+
+_NW = NumWordEN()
+
+def cardinal(value):
+    """
+    Convert to cardinal
+    """
+    return _NW.cardinal(value)
+
+def ordinal(value):
+    """
+    Convert to ordinal
+    """
+    return _NW.ordinal(value)
+
+def ordinal_number(value):
+    """
+    Convert to ordinal num
+    """
+    return _NW.ordinal_number(value)
+
+def currency(value, longval=True):
+    """
+    Convert to currency
+    """
+    return _NW.currency(value, longval=longval)
+
+def year(value, longval=True):
+    """
+    Convert to year
+    """
+    return _NW.year(value, longval=longval)
+
+def main():
+    """Main program"""
+    '''def check_and_convert_into_number(str):
+        #converting @str into float or long number
+        import math
+        try:
+            if re.search("[.,]",str) and not math.isnan(float(str)) or not math.isinf(float(str)):
+                return numword_en.cardinal(float(re.sub("[^\d.-]","",str))) # -asg30 ,.3879th -> -30.3879
+            elif not math.isnan(long(str)) or not math.isinf(long(str)):
+                canonic_number = long(re.sub("[^\d-]","",str)) # -asg30 3879 th -> -303879
+            else:
+                return False
+        except ValueError:
+            return False
+        if str.endswith(("th","rd","nd","st")):
+            result_words = ordinal(canonic_number)
+        elif str.endswith(("k")):
+            result_words = cardinal(canonic_number*1000)
+        elif str.endswith(("m")):
+            result_words = cardinal(canonic_number*1000000)
+        else:
+            result_words = re.sub("[\d.-]+",cardinal(canonic_number),str)
+        return result_words
+    import re
+
+    # todo dates "1980s", "'80s" -> eighties
+    decades = {"'20s":"twenties","1920s":"twenties","'30s":"thirties","1930s":"thirties","'40s":"fourties","1940s":"fourties",
+               "'50s":"fifties","1950s":"fifties","'60s":"sixties","1960s":"sixties","'70s":"seventies","1970s":"seventies",
+               "'80s":"eighties","1980s":"eighties","'90s":"nineties","1990s":"nineties"}
+    if "1980s" in decades:
+        print(decades[str])'''
+
+    for val in [ 1998, 11, 12, 21, 31, 33, 71, 80, 81, 91, 99, 100, 101, 102, 120, 155,
+             180, 300, 308, 832, 1000, 1001, 1061, 1100, 1120, 1500, 1701, 1800,
+             2000, 2010, 2099, 2171, 3000, 8280, 8291, 150000, 500000, 1000000,
+             2000000, 2000001, -21212121211221211111, -2.121212, -1.0000100,
+             1325325436067876801768700107601001012212132143210473207540327057320957032975032975093275093275093270957329057320975093272950730]:
+        _NW.test(val)
+        quit()
+
+if __name__ == "__main__":
+    main()
