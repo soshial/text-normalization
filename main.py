d5b098b3209c7c39a9bd510b22d1dede21611393
soshial <soshial.reg@gmail.com>
2012-08-17 13:50:09 +0400

    normalization: two_digit words, garbage sentences are omitted, interview transcripts cleaning, some lang-specific fixes

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 # coding: utf-8
+"""This file is quite vague script (sorry for that!), just an interface for all normalizing techniques,
+   programmed in num and numword classes"""
 __author__ = 'soshial'
 
 def dictionary_check(word):
@@ -56,7 +58,7 @@ def init_dic(lang):
     principal_words = {}
     other_words = {}
     cursor.execute ("SET NAMES 'utf8' COLLATE 'utf8_general_ci'")
-    cursor.execute ("SELECT word_id, word_spelling, principal FROM %s_word_equivalents WHERE lang = '%s' AND relation = 1 OR principal > 0" % (lang,lang))
+    cursor.execute ("SELECT word_id, word_spelling, principal FROM %s_word_equivalents WHERE lang = '%s' AND relation = 1 OR principal > 0 " % (lang,lang))
     while True:
         row = cursor.fetchone ()
         if row is None:
@@ -108,16 +110,29 @@ def restore_apostrophes(words,sentence):
         i+=1
     return words
 
+def split_twodigits(word):
+    """splitting two-digit words"""
+    numbers = {'decims':['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'],'units':['one','two','three','four','five','six','seven','eight','nine']}
+    word = unicode(word)
+    if re.search(' ',word): # composite word with spaces
+        word_ret = ''
+        for word_part in word.split(' '): word_ret += split_twodigits(word_part) + ' '
+        return word_ret.strip()
+    else: # any simple word without spaces
+        if not word == '' and re.search('-',word) and\
+                word.split('-')[0].lower() in numbers['decims'] and word.split('-')[1].lower() in numbers['units']:
+            return ' '.join(word.split('-')) # word with hyphens
+        else: return word # word without hyphens
+
 def abridgement_fix(words):
+    #print words;quit()
     i=0 # token counter
     days_of_week = {'Mon':'Monday','Tue':'Tuesday','Thu':'Thursday','Fri':'Friday','Sat':'Saturday'}
     months = {'Jan':'January','Feb':'February','Mar':'March','Apr':'April','Jun':'June','Jul':'July','Aug':'August','Sep':'September','Oct':'October','Nov':'November','Dec':'December'}
-    numbers = {'decims':['twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'],'units':['one','two','three','four','five','six','seven','eight','nine']}
     array_isdigit = lambda ar: (len(ar) == 1 and ar[0].isdigit()) or (len(ar) > 1 and (ar[0].isdigit() or array_isdigit(ar[1:])))
     for word in words:
         neighbourhood = words[max(0,i-4):min(i+4,len(words))]
-        # splitting two-digit words
-        if word.split('-')[0].lower() in numbers['decims'] and word.split('-')[1].lower() in numbers['units']: return word.split('-').join(' ')
+        word = split_twodigits(word)
         if (word == "#" or word == u"№") and i+1<len(words) and words[i+1].isdigit():
             words = words[:i]+[word+words[i+1]]+words[i+2:] # merging #1/№3
         # fixing time representation
@@ -152,8 +167,8 @@ def garbage_stats(words):
 def typographics(sentence):
     sentence = re.sub(re.compile(u"[`’']",re.UNICODE),u"'",sentence)
     sentence = re.sub(re.compile(u"[’‘’„“«»”]",re.UNICODE),u'"',sentence)
-    sentence = re.sub(re.compile(u"[‒]",re.UNICODE),u'–',sentence) # figure dash -> ndash
-    sentence = re.sub(re.compile(u"[―]",re.UNICODE),u'—',sentence) # horizontal bar -> mdash
+    sentence = re.sub(re.compile(u"[‒]",re.UNICODE),u'-',sentence) # figure dash -> ndash
+    sentence = re.sub(re.compile(u"[―]",re.UNICODE),u'-',sentence) # horizontal bar -> mdash
     sentence = re.sub(re.compile(u"[−–‐]",re.UNICODE),u'-',sentence) # only minus sign, only hyphen -> common hyphen-minus
     sentence = re.sub(re.compile(u"(\s?\.){3}",re.UNICODE),u'...',sentence) # ellipsis
     return sentence
@@ -166,7 +181,7 @@ path_in = arguments[2] # '/home/soshial/text-normalization/in/'
 path_out = arguments[3] # '/home/soshial/text-normalization/out/'
 import ConfigParser
 config = ConfigParser.RawConfigParser()
-config.read('normalization.cfg')
+config.read('/home/soshial/text-normalization/normalization.cfg')
 import re,regex
 
 # initializing logging
@@ -187,10 +202,12 @@ sent_detector = nltk_data.load('tokenizers/punkt/english.pickle')
 dictionary,principal_words,other_words = init_dic(lang)
 
 # working with files
-import glob, os, codecs
+import glob, os, codecs, time
 
 # main loop
 while True:
+    if glob.glob( os.path.join(path_in, '*.meta') ) == []: time.sleep(1); print "sleep" # if no files in a folder then sleep for a second
+    if os.path.isfile(path_in + "die"): os.remove(path_in + "die"); quit()
     for fullpath_inmeta in glob.glob( os.path.join(path_in, '*.meta') ):
         # file_in - input file, file_in2 - copy of input file, file_out - output file
         filename_intxt = fullpath_inmeta.split('/')[-1].split('.')[-2] + '.txt'
@@ -199,59 +216,61 @@ while True:
         file_intxt = codecs.open( path_in + filename_intxt, "r", "utf-8" )
         text_in = typographics(file_intxt.read()) # variable @file_intxt with our text to process
         if lang == "ru": # just adding all ru files to the 'file.list' for AOT to process
-            file_list = codecs.open(config.get('lms','aot_path') + "file.list", 'w+', 'utf-8-sig').write(filename_intxt+"\n").close()
+            file_list = codecs.open(config.get('lms','aot_path') + "ru_file.list", 'w+', 'utf-8-sig')
+            file_list.write(filename_intxt+"\n")
+            file_list.close()
         file_outtxt = codecs.open(path_out + filename_intxt, 'w', 'utf-8-sig') # output file as ./out/_____.out
         sentences = sent_detector.tokenize(text_in)
         for sentence_in in sentences:
-            sentence_out = ''; omit = False; i = 0
-            if re.search(u"[/|]",sentence_in): omit = True
-            # omitting the part of phrase that is considered as play/transcripts characters (MICHELE: ...)
-            # has : AND start is all upper OR is title AND first character after : is big
-            if regex.search(":",sentence_in) and (sentence_in.split(':')[0].isupper() or sentence_in.split(':')[0].istitle()) and sentence_in.split(':')[1].strip()[0].isupper():
-                sentence_in = ':'.join(sentence_in.split(':')[1:])
-            if regex.search(u"\p{L}{2,}[\.?!…]\p{L}{2,}",sentence_in):
-                omit = True
-                print "omitted because of the dot!"
-                logger.info('Dot inside the word problem with sentence:\n____'+sentence_in)
-                continue
-            words = PunktWordTokenizer().tokenize(sentence_in)
-            if words[-1].endswith('.'): words[-1]=words[-1][:-1] # removing dot in the end of the phrase
+            sentence_out = ''; omit = False; i = 0; words = []
             try:
+                if re.search(u"[/|]",sentence_in): omit = True
+                # omitting the part of phrase that is considered as play/transcripts characters (MICHELE: ...)
+                # has : AND start is all upper OR is title AND first character after : is big
+                if regex.search(":",sentence_in) and (sentence_in.split(':')[0].isupper() or sentence_in.split(':')[0].istitle()) and sentence_in.split(':')[1].strip()[0].isupper():
+                    sentence_in = ':'.join(sentence_in.split(':')[1:])
+                if regex.search(u"\p{L}{2,}[\.?!…]\p{L}{2,}",sentence_in):
+                    omit = True
+                    print "omitted because of the dot!"
+                    logger.info('Dot inside the word problem with sentence:\n____'+sentence_in)
+                    continue
+                words = PunktWordTokenizer().tokenize(sentence_in)
+                if words[-1].endswith('.'): words[-1]=words[-1][:-1] # removing dot in the end of the phrase
                 words = omit_brackets(words) # we don't need anything placed in brackets - it worsens LM
                 words = abridgement_fix(words)
                 words = restore_apostrophes(words,sentence_in) # word tokenizer designedly moves apart words with apostrophes inside them - we try fix this
+                if len(words) == 0:
+                    omit = True
+                # garbage concentration
+                if garbage_stats(words) > 1/3:
+                    omit = True
+                    logger.info('Garbage problem with sentence:\n____'+sentence_in)
+                    continue
             except Exception,ExText:
                 logger.info('Preprocessing exception: '+unicode(ExText))
-            if len(words) == 0:
-                omit = True
-            # garbage concentration
-            if garbage_stats(words) > 1/3:
-                omit = True
-                logger.info('Garbage problem with sentence:\n____'+sentence_in)
-                continue
             for word in words:
                 if re.search("\d",word): # if numbers are not present then we just return the string back
                     try:
-                        if i-1>0 and words[i-1] == "-": word = numw.check_and_convert_into_number("-"+word) # merging for negative numbers
+                        if i-1>=0 and words[i-1] == "-": word = numw.check_and_convert_into_number("-"+word) # merging for negative numbers
                         else: word = numw.check_and_convert_into_number(word)
+                        word = split_twodigits(word)
                     except StandardError, error_message:
                         omit = True
-                        logger.info('Problem with: ' + unicode(error_message) +':  '+ word)
+                        logger.info('Problem with : ' + unicode(error_message) +' // word: '+ unicode(word))
                         continue
                 if omit: break; # if something happens while converting, we should omit the sentence
                 if re.search(re.compile("\w",re.UNICODE),unicode(word)):
-                    temp_word = dictionary_check(word).upper()
-                    sentence_out += temp_word + ' '
+                    if lang == 'en': word = dictionary_check(word)
+                    sentence_out += word.upper() + ' '
                 i += 1
             if not omit:
                 sentence_out = re.sub(re.compile("[^\w. '-]",re.UNICODE),"",sentence_out) # todo \p{L} is not yet supported by Python 2.7.2
                 file_outtxt.write(sentence_out.strip() + "\n")
         file_outtxt.close()
         os.remove(fullpath_inmeta) # removing ./in/_____.meta for the loop not to process it again
-        os.remove(path_in + filename_intxt) # removing original ./in/_____.txt
+        #os.remove(path_in + filename_intxt) # removing original ./in/_____.txt
         file_meta = codecs.open(path_out + fullpath_inmeta.split('/')[-1], 'w', 'utf-8') # creating ./out/_____.meta
         file_meta.close()
-    #time.sleep(wait)
 
 # canonicalization:
 # "co-operation" → "cooperation", "valour" → "valor", "should've" → "should have" / redis?
